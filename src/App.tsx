660a533c99c5251965e211b57ee67e80019e049b
lucckkyGui <116285270+lucckkyGui@users.noreply.github.com>
2026-05-21 23:32:01 +0200

    Revert "Install and configure Vercel Speed Insights"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -51,7 +50,6 @@ const App = () => {
             </AuthProvider>
           </QueryClientProvider>
         </I18nProvider>
-        <SpeedInsights />
       </BrowserRouter>
     </ErrorBoundary>
   );
